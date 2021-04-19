a106ccc4d3193cd4d8f849a7bc5f4bcc25ff12c3
Eddie <eddie.ch.89d@gmail.com>
2021-04-19 15:01:00 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/eddie/registeration/RegisterationApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.eddie.registeration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
